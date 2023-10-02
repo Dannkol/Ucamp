d0d586dacdacaa6ce234dabfc63ed667dc49ee57
Daniel Manosalva <105187370+Dannkol@users.noreply.github.com>
2023-10-02 02:22:23 -0500

    feat: :poop: Example data endpoint
    dannkol

--- src/routes/v1/routes.js
+++ src/routes/v1/routes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 import session from 'express-session';
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 import { ensureAuthenticated } from '../../middleware/ensureAuthenticated.middleware.js';
 
@@ -30,6 +35,50 @@ Router_Init.get('/me', ensureAuthenticated, (req, res) => {
   res.status(200).json({
     message: 'ok'
   })
+});
+
+
+Router_Init.get('/cursosdefaul', (req, res) => {
+  // Aquí puedes manejar las acciones que deseas realizar en la ruta protegida
+  const filePath = path.join(__dirname+'../../../../db/cursos.json');
+
+  // Leer el archivo JSON de forma asíncrona utilizando promesas
+  fs.promises.readFile(filePath, 'utf8')
+    .then(data => {
+      // Analizar el contenido del archivo JSON
+      const jsonData = JSON.parse(data);
+
+      // Enviar los datos como respuesta
+      res.json(jsonData);
+    })
+    .catch(err => {
+      console.error('Error al leer el archivo JSON:', err);
+      res.status(500).json({ error: 'Error al leer el archivo JSON' });
+    });
+});
+
+Router_Init.get('/cursos', (req, res) => {
+  // Aquí puedes manejar las acciones que deseas realizar en la ruta protegida
+  const course= req.query.course;
+  console.log(course);
+  if(!course || course !== 'react') return res.status(403).json({
+    message: 'Archivo no encontrado'
+  });
+  const filePath = path.join(__dirname+'../../../../db/data.json');
+
+  // Leer el archivo JSON de forma asíncrona utilizando promesas
+  fs.promises.readFile(filePath, 'utf8')
+    .then(data => {
+      // Analizar el contenido del archivo JSON
+      const jsonData = JSON.parse(data);
+
+      // Enviar los datos como respuesta
+      res.json(jsonData);
+    })
+    .catch(err => {
+      console.error('Error al leer el archivo JSON:', err);
+      res.status(500).json({ error: 'Error al leer el archivo JSON' });
+    });
 });
 
 export { Router_Init }
